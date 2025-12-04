5236d068c0ba5f18abb57dabdd1efd6d65b63eab
BoringTrick <landonmorrical@gmail.com>
2025-12-03 22:30:33 -0600

    zoom in/out were reversed by accident

--- scenes/cameracontrol.gd
+++ scenes/cameracontrol.gd
@@ -4,11 +4,11 @@ const ZOOM_MAX = Vector2(0.1, 0.1)
 const ZOOM_MIN = Vector2(2,2)
 
 func _process(delta):
-	if Input.is_action_just_released("zoomIn") and zoom > ZOOM_MAX:
-		zoom /= Vector2(1.5,1.5)
-		
-	if Input.is_action_just_released("zoomOut") and zoom < ZOOM_MIN:
+	if Input.is_action_just_released("zoomIn") and zoom < ZOOM_MIN :
 		zoom *= Vector2(1.5,1.5)
+		
+	if Input.is_action_just_released("zoomOut") and zoom > ZOOM_MAX:
+		zoom /= Vector2(1.5,1.5)
 		
 	if Input.is_action_pressed("panLeft"):
 		offset.x -= 300 * delta
